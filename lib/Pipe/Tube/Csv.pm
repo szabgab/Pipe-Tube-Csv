7a68853c050aaac5ec6933e8f99213d4f316e7c2
Gabor Szabo <szabgab@gmail.com>
2006-06-06 05:36:37 +0000

    release of version 0.01

--- lib/Pipe/Tube/Csv.pm
+++ lib/Pipe/Tube/Csv.pm
@@ -2,30 +2,77 @@ package Pipe::Tube::Csv;
 use strict;
 use warnings;
 
-use base 'Pipe::Tube';
+our $VERSION = '0.01';
 
-# split up lines of csv file ? and return an array ?
-# use the first row as key names and on every other row return a hash of the values ?
+use base 'Pipe::Tube';
+use Text::CSV_XS;
+use Data::Dumper;
+
 
 sub init {
-    my ($self, $expr) = @_;
-    $self->logger("Receiving Csv definition");
-    $self->{expr} = $expr;
+    my ($self, $attr) = @_;
+    die "First paramater of csv should be HASH reference or nothing" if $attr and ref $attr ne 'HASH';
+
+    $self->logger("Receiving Csv definition: " . Dumper $attr);
+
+    $self->{csv} = Text::CSV_XS->new($attr);
     return $self;
 }
 
 sub run {
     my ($self, @input) = @_;
 
-    $self->logger("The grep expression: $self->{expr}");
-    if ("Regexp" eq ref $self->{expr}) {
-        return grep /$self->{expr}/, @input;
-    } else {
-        my $sub = $self->{expr};
-        return grep { $sub->($_) } @input;
+    my @resp;
+    foreach my $line (@input) {
+      $self->{csv}->parse($line);
+      push @resp, [ $self->{csv}->fields ];
     }
+    return @resp;
 }
 
-
-
 1;
+
+__END__
+
+=head1 NAME
+
+Pipe::Tube::Csv - Csv processor tube in Pipe
+
+=head1 SYNPOSIS
+
+  my @resp = Pipe->for(@rows)->csv;
+
+  my @resp = Pipe->cat("t/data/file1", "t/data/file2")->csv({ sep_char => "\t" });
+
+=head1 DESCRIPTION
+
+The ->csv()  call can get a HASH reference parameter, the same parameter as 
+L<Text::CSV_XS> would get. We pass it directly to that module.
+
+Split up lines of csv file and return an array reference for each line.
+
+TODO: use the first row as key names and on every other row return a hash of the values
+ using the above header
+
+
+=head1 AUTHOR
+
+Gabor Szabo <gabor@pti.co.il>
+
+=head1 COPYRIGHT
+
+Copyright 2006 by Gabor Szabo <gabor@pti.co.il>.
+
+This program is free software; 
+you can redistribute it and/or modify it under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+
+=head1 See Also
+
+L<Pipe> and L<Text::CSV_XS>
+
+
+=cut
+
+
